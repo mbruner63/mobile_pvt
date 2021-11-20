de8b619d6820896261b4a8f66c3e565c80f56a96
Martin Bruner <marty@bruner-consulting.com>
2021-11-20 10:42:32 -0700

    Revert "Tuda branch1"

--- lib/menu_screen.dart
+++ lib/menu_screen.dart
@@ -22,8 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primaryColor: Colors.blue.shade800,
-        //primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
